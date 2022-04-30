e0f481e8b6a567430ea41138a67d26bbbe37db29
Bartlomiej Kopysc <bkopysc@wp.pl>
2022-04-30 23:46:39 +0200

    added links

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onbeforeunload = function () {
-    window.scrollTo(0, 0);
-  }
+// window.onbeforeunload = function () {
+//     window.scrollTo(0, 0);
+//   }
 
 function selectWebTabs(){
     webTabs =  document.getElementById("web-tabs");
